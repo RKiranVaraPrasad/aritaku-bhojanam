f8de06d47e07f03cc53fb66598c3091b26ff2eac
Kiran <kiran.varaprasad@gmail.com>
2021-02-19 12:50:19 +0530

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './featured/home/home.module';
 import { CoreModule } from './core/core.module';
+import { MenuModule } from './featured/menu/menu.module';
+import { AboutModule } from './featured/about/about.module';
+import { ServiceModule } from './featured/service/service.module';
+import { ContactModule } from './featured/contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     BrowserAnimationsModule,
     HomeModule,
+    AboutModule,
+    MenuModule,
+    ServiceModule,
+    ContactModule,
     CoreModule,
     AppRoutingModule
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatInputModule } from '@angular/material/input';
 import { FooterComponent } from './components/footer/footer.component';
-
-
-
-
 
 @NgModule({
   declarations: [PageNotFoundComponent, NavbarComponent, FooterComponent],
@@ -24,6 +19,7 @@ import { FooterComponent } from './components/footer/footer.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatMenuModule,
     MatDatepickerModule,
     MatNativeDateModule
   ],
